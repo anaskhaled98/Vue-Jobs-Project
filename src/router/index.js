eeddab7f7280a2c97c15790e9829bf6052f944da
anas khaled <anaskhaled98@gmail.com>
2024-11-19 18:12:26 +0200

    - Add Job Implementation.

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import { createWebHistory, createRouter } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import JobsView from "@/views/JobsView.vue";
 import NotFoundView from "../views/NotFoundView.vue";
+import AddJob from "../views/AddJob.vue";
 import JobView from "../views/JobView.vue";
 
 const routes = [
   { path: "/", name: "home", component: HomeView },
   { path: "/jobs", name: "jobs", component: JobsView },
   { path: "/jobs/:id", name: "job", component: JobView },
+  { path: "/jobs/add", name: "add-job", component: AddJob },
   { path: "/:catchAll(.*)", name: "not-found", component: NotFoundView },
 ];
 
